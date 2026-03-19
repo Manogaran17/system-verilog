1c81d262f33b9c9a96ccd714b079c51eddde0a79
Manogaran17 <manogaran2667@gmail.com>
2026-03-19 23:49:29 +0530

    Update Four state data types.sv

--- 01-DATA TYPES/02 - FOUR STATE DATA TYPES/Four state data types.sv
+++ 01-DATA TYPES/02 - FOUR STATE DATA TYPES/Four state data types.sv
@@ -1,1 +1,44 @@
+// Code your testbench here
+// or browse Examples
 
+module two_state_datatype;
+  logic [7:0]a;
+  reg [6:0]b;
+  wire[3:0]c;
+  real d;
+  time e;
+  integer f;
+
+  
+  initial 
+    begin
+      $display("default value of a=%b",a);
+      $display("default value of b=%b",b);
+      $display("default value of c=%b",c);
+      $display("default value of d=%0f",d);
+      $display("default value of e=%0t",e);
+      $display("default value of f=%b",f);
+    end 
+  
+initial 
+    begin
+  #20;
+  a=8'hff;
+  b=6'd34;
+  c=4'd9;
+  d=26.67;
+  e=$time;
+  f=64'hffffffffffffffff;
+
+      $display("default value of a=%b",a);
+      $display("default value of b=%b",b);
+      $display("default value of c=%b",c);
+      $display("default value of d=%0f",d);
+      $display("default value of e=%0t",e);
+      $display("default value of f=%b",f);
+    end 
+  
+  
+endmodule 
+  
+      
