71d39c9b3cb952760eee1da8d40dc287f226979a
Manogaran17 <manogaran2667@gmail.com>
2026-04-07 23:40:00 +0530

    Update 01-PACKED.sv

--- 01-DATA TYPES/03-STRUCTURE/01-PACKED.sv
+++ 01-DATA TYPES/03-STRUCTURE/01-PACKED.sv
@@ -1,23 +1,43 @@
 // Code your testbench here
 // or browse Examples
-module packed_struct;
+
+module structure;
   
-  struct packed {
-    int a;
-    logic [7:0]b;
-    bit [7:0]c;
-    byte d;
-  }data;
+  typedef struct {
+    string name;
+    shortint rollno;
+    int mark;
+  }result;
+  
+  result student[3];
   
   initial 
     begin
-      data.a=45;
-      data.b=76;
-      data.c=67;
-      data.d=88;
-      $display("value of a=%0d",data.a);
-      $display("value of b=%0d",data.b);
-      $display("value of c=%0d",data.c);
-      $display("value of d=%0d",data.d);
+      
+      student[0].name="mano";
+      student[0].rollno=29;
+      student[0].mark=564;
+      
+      student[1].name="sakthi";
+      student[1].rollno=38;
+      student[1].mark=593;
+      
+      student[2]='{name:"ram",mark:573,rollno:27};
+      
+      $display("\n------student details----------");
+      
+      foreach(student[i])begin
+        $display("\nstudents %0d:",i);
+        $display("name=%s",student[i].name);
+        $display("rollno=%0d",student[i].rollno);
+        $display("mark=%0d",student[i].mark);
+      end 
+    
+      $display("actual array=%p",student);
     end 
 endmodule 
+      
+      
+      
+    
+    
