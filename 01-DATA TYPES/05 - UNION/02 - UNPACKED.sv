05e72e5b61f20ed549784b0e54e74957cde6ac7f
Manogaran17 <manogaran2667@gmail.com>
2026-04-11 15:41:51 +0530

    Update 02 - UNPACKED.sv

--- 01-DATA TYPES/05 - UNION/02 - UNPACKED.sv
+++ 01-DATA TYPES/05 - UNION/02 - UNPACKED.sv
@@ -1,1 +1,37 @@
-
+// Code your testbench here
+// or browse Examples
+module union_unpacked;
+  
+  typedef union{
+    
+    int i;
+    logic[7:0]data;
+    shortint data1;
+    //string s;
+  }uni;
+  
+  uni u;
+  
+  initial 
+    begin
+      
+      $display("======displaying integer value=======");
+      
+      u.i=32'haabbccdd;
+      $display("i = %0d",u.i);
+      
+      // integer value after modification 
+      
+      u.i[31:24]=8'hff;
+      $display("after modification of int i=%0h",u.i);
+     
+      u.data=76;
+      $display("data =%0d",u.data);
+      
+      $display("u=%p",u);
+      
+    end 
+  
+endmodule 
+      
+      
