af26e24f65e8e8b7ee3a1e45e032f62ec6cca222
Manogaran17 <manogaran2667@gmail.com>
2026-05-31 23:20:26 +0530

    Create 08-PRIORITY_CASE.sv

---
+++ 05-CONTROL FLOW/08-PRIORITY_CASE.sv
@@ -0,0 +1,58 @@
+module unique_case;
+  
+  logic [9:0] marks;
+  
+  function void grade (input logic [9:0]marks);
+    
+    priority case(1)
+      
+      (marks>=90&&marks<=100):
+        $display("Grade A");
+      
+      (marks>=75):
+        $display("Grade B");
+      
+      (marks>=50&&marks<75):
+        $display("Grade C");
+      
+      (marks<40):
+        $display("FAIL");
+      
+    endcase
+      
+      endfunction 
+  
+ initial begin
+
+    $display("\n======= unique case example =======");
+
+    marks = 98;
+    grade(marks);
+
+    marks = 65;
+    grade(marks);
+
+    marks = 34;
+    grade(marks);
+
+    marks = 89;
+    grade(marks);
+   
+    marks = 42 ;
+    grade(marks);
+
+  end
+
+endmodule   
+======= unique case example =======
+Grade A
+Grade C
+FAIL
+Grade B
+
+Warning-[RT-NCMPRCS] No condition matches in statement
+testbench.sv, 7
+  No condition matches in 'priority case' statement. 'default' specification 
+  is missing, inside unique_case.grade, at time 0ns.
+
+           V C S   S i m u l a t i o n   R e p o r t 
