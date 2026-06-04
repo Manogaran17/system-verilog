e928844824474cbb4a8c999bab424f77c149fc07
Manogaran17 <manogaran2667@gmail.com>
2026-06-04 16:39:11 +0530

    Update 05-DEFAULT CONSTRUCTOR.sv

--- 06-CLASSES AND OOPS/05-DEFAULT CONSTRUCTOR.sv
+++ 06-CLASSES AND OOPS/05-DEFAULT CONSTRUCTOR.sv
@@ -1,1 +1,36 @@
+// Code your testbench here
+// or browse Examples
+class transaction;
+  
+  int addr;
+  int data;
+  
+  function new();
+    addr = 8'haa;
+    data = 8'hff;
+    $display("\nconstructor called");
+  endfunction 
+  
+endclass
 
+module top;
+  
+  transaction tr;
+  
+  initial 
+    begin
+      
+      tr = new();
+      
+      $display("\nadd = %0h",tr.addr);
+      $display("\ndata = %0h",tr.data);
+      
+    end 
+endmodule 
+// ==========================================output===========================================
+constructor called
+
+add = aa
+
+data = ff
+           V C S   S i m u l a t i o n   R e p o r t 
