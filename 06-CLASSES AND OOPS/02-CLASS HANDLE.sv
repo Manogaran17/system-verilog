51e90c9057c94f5b6184e842c41b32273a8f9c61
Manogaran17 <manogaran2667@gmail.com>
2026-06-04 14:55:29 +0530

    Create 02-CLASS HANDLE.sv

---
+++ 06-CLASSES AND OOPS/02-CLASS HANDLE.sv
@@ -0,0 +1,41 @@
+// Code your testbench here
+// or browse Examples
+
+class home;  // define class 
+  
+//   class propreties 
+    
+  bit light;
+  int fan;
+  string switch;
+  
+//     class methods
+  task open_electricity();
+    switch = "ON";
+    $display("switch is %s electricity is open",switch);
+  endtask
+  
+endclass
+
+
+module tb;
+  
+  home h1; 
+  
+  initial 
+    begin
+        
+      h1 = new();
+      $display("_______________________________________________");
+      
+      $display("check wheather the object is created or not");
+      if(h1==null)
+        $display("object is not created");
+      else
+        begin
+        $display("object is created");
+        end 
+       $display("_______________________________________________");
+      
+    end 
+endmodule 
