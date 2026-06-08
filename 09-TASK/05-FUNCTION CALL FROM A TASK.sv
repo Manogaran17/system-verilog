a669fe25306f292e35019aa302b05318e9d59b0f
Manogaran17 <manogaran2667@gmail.com>
2026-06-08 09:58:05 +0530

    Create 05-FUNCTION CALL FROM A TASK.sv

---
+++ 09-TASK/05-FUNCTION CALL FROM A TASK.sv
@@ -0,0 +1,59 @@
+// Code your testbench here
+// or browse Examples
+module tb;
+  function automatic int calc_crc(input int addr,input int data);                               
+    int crc;
+    crc=(addr^data)+8'hA3;
+    return crc;
+  endfunction
+
+  task automatic send_packet(input int addr,input int data);
+                            
+    int crc;
+    int packet;
+    $display("\n==============================");
+    $display("TASK:Sending Packet");
+    $display("ADDR=%0d|DATA=%0d",addr,data);
+
+    crc=calc_crc(addr,data); // calling a function from task
+    packet={addr[7:0],data[7:0],crc[7:0]};
+
+    $display("CRC=%0d",crc);
+    $display("PACKET=%0h",packet);
+    $display("==============================");
+
+  endtask
+
+  initial 
+    begin
+      send_packet(10,20); // calling a task 
+    send_packet(25,40);
+    send_packet(100,55);
+  end
+endmodule
+// ==========================================OUTPUT==========================================================
+==============================
+TASK:Sending Packet
+ADDR=10|DATA=20
+CRC=193
+PACKET=a14c1
+==============================
+
+==============================
+TASK:Sending Packet
+ADDR=25|DATA=40
+CRC=212
+PACKET=1928d4
+==============================
+
+==============================
+TASK:Sending Packet
+ADDR=100|DATA=55
+CRC=246
+PACKET=6437f6
+==============================
+           V C S   S i m u l a t i o n   R e p o r t 
+Time: 0 ns
+CPU Time:      0.570 seconds;       Data structure size:   0.0Mb
+Mon Jun  8 00:23:25 2026
+Done
