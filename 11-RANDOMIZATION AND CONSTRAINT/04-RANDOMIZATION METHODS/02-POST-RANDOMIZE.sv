3313988acbcf8a345e18ac61404e7b601e156cae
Manogaran17 <manogaran2667@gmail.com>
2026-06-11 15:16:42 +0530

    Update 02-POST-RANDOMIZE.sv

--- 11-RANDOMIZATION AND CONSTRAINT/04-RANDOMIZATION METHODS/02-POST-RANDOMIZE.sv
+++ 11-RANDOMIZATION AND CONSTRAINT/04-RANDOMIZATION METHODS/02-POST-RANDOMIZE.sv
@@ -1,39 +1,92 @@
-// Code your testbench here
-// or browse Examples
+class packet;
 
-class generator;
+  rand bit [7:0] addr;
+  rand bit [7:0] data;
 
-  rand bit[3:0]value;
-  int status;
-  int double_value;
+  bit write;
+  constraint addr_c {
+    addr inside {[10:20]};
+  }
+  constraint data_c {
+    data inside {[100:150]};
+  }
 
-  constraint c1{value inside{[1:10]}; }
+  
+  function void pre_randomize();
+    $display("\nInside pre_randomize()");
+    write = $urandom_range(0,1);
+    
+    if(write)
+      $display("WRITE transaction selected");
+    else
+      $display("READ transaction selected");
+  endfunction
 
+  
   function void post_randomize();
-    $display("Inside post_randomize");
-    status=1;
-    double_value=value*2;
+    $display("Inside post_randomize()");
+    if(write)
+      data = data + 10;
   endfunction
 endclass
 
 module tb;
-
-  generator gen;
+  packet p;
   initial 
     begin
-    gen=new();
-    repeat(3)
+    p=new();
+    repeat(5)
       begin
-      gen.randomize();
-      $display("value=%0d status=%0d double=%0d",gen.value,gen.status,gen.double_value);
-    end
+        $display("\nCalling randomize()");
+        void'(p.randomize());
+        $display("WRITE = %0d",p.write);
+        $display("ADDR  = %0d",p.addr);
+        $display("DATA  = %0d",p.data);
+      end
   end
 endmodule
-// ============================OUTPUT============================================
-Inside post_randomize
-value=3 status=1 double=6
-Inside post_randomize
-value=3 status=1 double=6
-Inside post_randomize
-value=7 status=1 double=14
-           V C S   S i m u l a t i o n   R e p o r t 
+// ==================================OUTPUT===========================================
+Calling randomize()
+
+Inside pre_randomize()
+READ transaction selected
+Inside post_randomize()
+WRITE = 0
+ADDR  = 12
+DATA  = 147
+
+Calling randomize()
+
+Inside pre_randomize()
+WRITE transaction selected
+Inside post_randomize()
+WRITE = 1
+ADDR  = 12
+DATA  = 111
+
+Calling randomize()
+
+Inside pre_randomize()
+READ transaction selected
+Inside post_randomize()
+WRITE = 0
+ADDR  = 16
+DATA  = 136
+
+Calling randomize()
+
+Inside pre_randomize()
+WRITE transaction selected
+Inside post_randomize()
+WRITE = 1
+ADDR  = 18
+DATA  = 135
+
+Calling randomize()
+
+Inside pre_randomize()
+WRITE transaction selected
+Inside post_randomize()
+WRITE = 1
+ADDR  = 12
+DATA  = 131
